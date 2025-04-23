5c6b5e95738be7cdb4116f2cc79d5991abbb41e3
s4012930 <s4012930@student.rmit.edu.au>
2025-04-23 14:23:19 +1000

    Foundations of the Popup

---
+++ src/popup/App.tsx
@@ -0,0 +1,9 @@
+export default function Popup() {
+    return (
+      <main className="p-4 w-80">
+        <h1 className="text-xl font-bold mb-4">Hello! Testing!</h1>
+        <p className="text-sm">Tailwind testing</p>
+      </main>
+    );
+  }
+  
